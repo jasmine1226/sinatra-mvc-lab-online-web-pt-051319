27fe755409678370b1a4573f3f30aebd192a1194
Jasmine Chen <jasmine1226@gmail.com>
2019-08-17 22:44:16 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,22 +6,18 @@ class PigLatinizer
   end
 
   def piglatinize(text)
-    text.split(" ").collect do |word|
-      consonant_check = false
-      word.downcase.split("").each do |c|
-        if c.match(/[bcdfghjklmnpqrstvwxyz]/)
-          word += word[0]
-          word.slice!(0)
-          consonant_check = true
-        elsif c.match(/[aeiou]/)
-          word += 'w' if !consonant_check
-          word += 'ay'
-          break
-        end
-      word
+    consonant_check = false
+    word.downcase.split("").each do |c|
+      if c.match(/[bcdfghjklmnpqrstvwxyz]/)
+        word += word[0]
+        word.slice!(0)
+        consonant_check = true
+      elsif c.match(/[aeiou]/)
+        word += 'w' if !consonant_check
+        word += 'ay'
+        break
       end
+    word
     end
-    text.join(" ")
   end
-
 end
